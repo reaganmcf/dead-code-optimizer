50f8ee8da6cfae055418391a6ad803230c84b6c0
Reagan McFarland <me@reaganmcf.com>
2022-04-24 17:30:50 -0400

    fix: testcase output file fixes

--- test.fish
+++ test.fish
@@ -1,14 +1,14 @@
 #!/usr/bin/fish
 
 for f in testcases/test*.iloc
-	python3 deadcode.py < $f > testcases/opt-(basename $f).iloc
+	python3 deadcode.py < $f > testcases/opt-(basename $f)
 	echo "Running " $f
 	echo "Before"
 	../ILOC_Simulator/sim < $f 
 	
 	echo "--"
 	echo "After"
-	../ILOC_Simulator/sim < testcases/opt-(basename $f).iloc
+	../ILOC_Simulator/sim < testcases/opt-(basename $f)
 
 	echo "----------"
 end
